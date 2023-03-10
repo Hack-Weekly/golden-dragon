7401d7ccf6b10058cef80969064bbd8cde9cb9c5
taddymason <67209663+kingl0w@users.noreply.github.com>
2023-03-10 12:28:39 -0800

    todo js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Script to add todo items and clear input value after //
 let addToDoBtn = document.getElementById('todo-add');
-let taskList = document.getElementById('tasks');
+let todoList = document.getElementById('todoItems');
 let inputToDo = document.getElementById('todo-input');
 
 addToDoBtn.addEventListener('click', function(){
     var paragraph = document.createElement('p')
     paragraph.innerText = inputToDo.value;
-    taskList.appendChild(paragraph);
+    todoList.appendChild(paragraph);
     inputToDo.value = "";
 });
 
