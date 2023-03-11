adcdc18326ba2530eed7d5ef4e71493d9fe05f5b
diego-rapoport <diegomrapoport@gmail.com>
2023-03-10 22:18:11 -0300

    Add done and enter key to add

--- script.js
+++ script.js
@@ -3,10 +3,28 @@ let addToDoBtn = document.getElementById('todo-add');
 let todoList = document.getElementById('todoItems');
 let inputToDo = document.getElementById('todo-input');
 
-addToDoBtn.addEventListener('click', function(){
-    var paragraph = document.createElement('p')
-    paragraph.innerText = inputToDo.value;
-    todoList.appendChild(paragraph);
-    inputToDo.value = "";
+function addDoneButton() {
+  const newButton = document.createElement('button')
+  newButton.innerHTML = 'DONE'
+  newButton.addEventListener('click', (event) => {
+    event.target.previousElementSibling.style.textDecorationLine = 'line-through'
+    event.target.remove()
+  })
+  return newButton
+}
+
+addToDoBtn.addEventListener('click', function() {
+  const outDiv = document.createElement('div')
+  outDiv.classList.add('todoDiv')
+  const paragraph = document.createElement('p')
+  const newButton = addDoneButton()
+  paragraph.innerText = inputToDo.value;
+  outDiv.appendChild(paragraph)
+  outDiv.appendChild(newButton)
+  todoList.appendChild(outDiv);
+  inputToDo.value = "";
 });
 
+inputToDo.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') addToDoBtn.click()
+})
