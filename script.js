5ddaef2ff08202aadb7a2ff242f41aadc594c655
Vinit Shah <vinitshah9942@gmail.com>
2023-03-12 22:55:36 -0500

    Added 'Reset' button to clear all task

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 let addToDoBtn = document.getElementById("todo-add");
 let todoList = document.getElementById("todoItems");
 let inputToDo = document.getElementById("todo-input");
+let toDoForm = document.getElementById("todo-form");
+let toDoContainer = document.getElementById("todo-container");
+const resetButton = document.createElement('button')
+let itemsCount = 0
 
 function addDoneButton() {
   const newButton = document.createElement('button')
@@ -14,6 +18,7 @@ function addDoneButton() {
   return newButton
 }
 
+
 addToDoBtn.addEventListener('click', function() {
   if(inputToDo.value === '') return
   const outDiv = document.createElement('div')
@@ -25,8 +30,37 @@ addToDoBtn.addEventListener('click', function() {
   outDiv.appendChild(newButton)
   todoList.appendChild(outDiv);
   inputToDo.value = "";
+  if (itemsCount === 0) {
+    addResetButton()
+  }
+  itemsCount += 1
+  
 });
+
+function resetOnClick () {
+  todoList = document.getElementById("todoItems")
+  const childerElement = todoList.childNodes
+  while (childerElement.length >0) {
+    todoList.removeChild(childerElement[0])
+  }
+  resetButton.remove()
+  itemsCount = 0
+}
+
+function addResetButton () {
+  resetButton.setAttribute('id', 'reset-button')
+  resetButton.setAttribute('onclick', 'resetOnClick()')
+  resetButton.innerText = "Reset"
+  toDoForm.appendChild(resetButton)
+}
 
 inputToDo.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') addToDoBtn.click()
 })
+
+function addTodoItems () {
+  const itemsDiv = document.createElement('div')
+  itemsDiv.setAttribute('id', 'todoItems')
+  toDoContainer.appendChild(itemsDiv)
+}
+
