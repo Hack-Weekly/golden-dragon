2075d09ae2ff1fa84f7870f0985d1ea1819f69b5
diego-rapoport <diegomrapoport@gmail.com>
2023-03-12 22:30:17 -0300

    Fix todo size

--- script.js
+++ script.js
@@ -1,51 +1,54 @@
-// Script to add todo items and clear input value after //
 let addToDoBtn = document.getElementById('todo-add');
 let todoList = document.getElementById('todoItems');
 let inputToDo = document.getElementById('todo-input');
 
-function addDoneButton() {
-  const newButton = document.createElement('button');
-  newButton.innerHTML = 'DONE';
-  newButton.addEventListener('click', (event) => {
-    event.target.previousElementSibling.style.textDecorationLine =
-      'line-through';
+function addDoneTodo(todo) {
+  const doneBtn = document.createElement('button');
+  doneBtn.innerText = 'DONE';
+  doneBtn.addEventListener('click', (event) => {
+    todo.style.textDecorationLine = 'line-through';
     event.target.remove();
   });
-  return newButton;
+  return doneBtn;
 }
 
-addToDoBtn.addEventListener('click', function () {
+function addDeleteTodo(todo) {
+  const iconDelete = document.createElement('iconify-icon')
+  iconDelete.setAttribute('icon', 'bi:trash')
+
+  iconDelete.addEventListener('click', () => todo.remove());
+  return iconDelete
+}
+
+function buttonsDiv(todo) {
+  const butnsDiv = document.createElement('div')
+  butnsDiv.classList.add('todo-buttons-div')
+
+  const doneBtn = addDoneTodo(todo)
+  const delBtn = addDeleteTodo(todo)
+
+  butnsDiv.append(doneBtn, delBtn)
+
+  return butnsDiv
+}
+
+addToDoBtn.addEventListener('click', function() {
   if (inputToDo.value !== '') {
     const todoItem = document.createElement('li');
     const todoText = document.createElement('p');
-    const doneBtn = addDoneButton();
+    todoText.classList.add('todo-text')
 
     todoText.innerText = inputToDo.value;
-    
-    todoItem.classList.add('todoDiv');
 
-    todoItem.appendChild(todoText);
-    todoItem.appendChild(doneBtn);
-    todoDelete(todoItem);
-    todoList.appendChild(todoItem);
+    todoItem.classList.add('todoDiv');
+    const butnsDiv = buttonsDiv(todoItem)
+
+    todoItem.append(todoText, butnsDiv);
+    todoList.appendChild(todoItem)
 
     inputToDo.value = '';
   }
 });
-
-function todoDelete(todo) {
-  const deleteTodoBtn = document.createElement('button');
-
-  deleteTodoBtn.innerHTML = `
-    <iconify-icon
-      icon="bi:trash"
-      style="color: white;"
-    ></iconify-icon>`;
-
-  deleteTodoBtn.addEventListener('click', () => todo.remove());
-
-  todo.appendChild(deleteTodoBtn);
-}
 
 inputToDo.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') addToDoBtn.click();
